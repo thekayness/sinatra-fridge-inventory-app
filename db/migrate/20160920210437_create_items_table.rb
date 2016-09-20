31af29d7f956c275198872ef0b50c55c4fa50221
Kathleen DaSilva <kayjsmyth@gmail.com>
2016-09-20 21:13:53 +0000

    updated migrations

--- db/migrate/20160920210437_create_items_table.rb
+++ db/migrate/20160920210437_create_items_table.rb
@@ -1,4 +1,9 @@
 class CreateItemsTable < ActiveRecord::Migration
   def change
+    create_table :items do |t|
+      t.string :name
+      t.string :exp_date
+      t.string :category
+    end
   end
 end
--- db/migrate/20160920210753_create_user_items_table.rb
+++ db/migrate/20160920210753_create_user_items_table.rb
@@ -1,4 +1,8 @@
 class CreateUserItemsTable < ActiveRecord::Migration
   def change
+    create_table :user_items do |t|
+      t.belongs_to :user, index: true
+      t.belongs_to :item, index: true
+    end
   end
 end
