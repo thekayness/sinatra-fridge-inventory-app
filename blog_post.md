b2e75793e74a6bc6b6750284202f62422944554a
Kathleen DaSilva <kayjsmyth@gmail.com>
2016-09-21 15:05:57 +0000

    working on specs

---
+++ blog_post.md
@@ -0,0 +1,10 @@
+Thoughts:
+Think of how your moving parts fit into the bigger picture/context of things you've learned.
+
+Feel the synergy, how is something you want to create emulating something you've already made?
+
+Reduce, reuse, recycle!!!
+-file tree structure (app>mvc, config, db) to specific methods (slugify)
+-sounds like cheating, but learning how to imagine new things 
+within the framework of your current knowledge and relate it to something already in existence is working those concepts!
+-connect the dots, maybe in the same ways, maybe in new ways!
---
+++ spec/models_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe 'User' do
+  before do
+    @user = User.create(:username => "test 123", :email => "test123@aol.com", :password => "test")
+
+    @item =  Item.create(:name => "Tomato sauce", :exp_date => "9/9/1999")
+  end
+
+
+  it 'can slug the username' do
+    expect(@user.slug).to eq("test-123")
+  end
+
+  it 'can find a user based on the slug' do
+    slug = @user.slug
+    expect(User.find_by_slug(slug).username).to eq("test 123")
+  end
+
+  it 'has a secure password' do
+
+    expect(@user.authenticate("dog")).to eq(false)
+    expect(@user.authenticate("test")).to eq(@user)
+
+  end
+end
+
+describe "item" do
+
+  it "can initialize a item" do
+    expect(Item.new).to be_an_instance_of(Item)
+  end
+
+  it "can have a name" do
+    expect(@item.name).to eq("Tomato sauce")
+  end
+
+  it "can have an expiration date" do
+    expect(@item.exp_date).to eq("9/9/1999")
+  end
+
+  it "can have many users" do
+    expect(UserItems.count).to eq(1)
+  end
+
+  it "can slugify its name" do
+
+    expect(@item.slug).to eq("tomato-sauce")
+  end
+
+  describe "Class methods" do
+    it "given the slug can find a item" do
+      slug = "tomato-sauce"
+
+      expect((Item.find_by_slug(slug)).name).to eq("Tomato sauce")
+    end
+  end
+end
